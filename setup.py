eb3ec080516ac7d23b5df53ab5a9689692c8126c
Adam <PseudoDesign@users.noreply.github.com>
2018-11-12 12:51:07 -0800

    removing flag altogether

--- setup.py
+++ setup.py
@@ -36,18 +36,10 @@ crcmod_ext = Extension('crc32c',
 def get_extra_compile_args():
     # msvc is treated specially; otherwise we assume it's a unix compiler
     comp = distutils.ccompiler.get_default_compiler()
-    sw_compile = ""
-    if "CRC32C_SW_MODE" in os.environ:
-        sw_compile = os.environ['CRC32C_SW_MODE']
-    if sw_compile != "force":
-        sw_compile = ""
     if comp == 'msvc':
         return ['/O2', '/DNDEBUG']
     else:
-        if sw_compile == "force":
-            return ['-O3', '-DNDEBUG']
-        else:
-            return ['-O3', '-msse4.2', '-mpclmul', '-DNDEBUG']
+        return ['-O3', '-DNDEBUG']
 
 class _build_ext(build_ext):
     """Custom build_ext command that includes extra compilation arguments"""
